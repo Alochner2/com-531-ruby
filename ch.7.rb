182a78c972ad888c53db836cf5c265f66b7cc6f7
Amber Lochner <alochner@hawk.iit.edu>
2020-09-14 18:08:26 -0500

    Begin work on branching in Ch. 7

---
+++ ch.7.rb
@@ -0,0 +1,25 @@
+puts 1 > 2
+puts 1 < 2
+puts 5 >= 5
+puts 5 <= 4
+puts 1 == 1
+puts 2 != 1
+puts 'cat' < 'dog'
+puts 'bug lady' < 'Xander'
+puts 'bug lady'.downcase < 'Xander'.downcase
+puts 'Hello, what\'s your name?'
+name = gets.chomp
+puts 'Hello, ' + name + '.'
+
+if name == 'Chris'
+  puts 'What a lovely name!'
+end
+
+puts 'I am a fortune-teller. Tell me your name:'
+name = gets.chomp
+if name == 'Chris'
+  puts 'I see great things in your future.'
+else
+  puts 'Your future is...oh my! Look at the time!'
+  puts 'I really have to go, sorry!'
+end
