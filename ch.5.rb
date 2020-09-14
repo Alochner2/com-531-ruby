cdd46ac32bb283ea3f9ea0e8652b34268bebbf7a
Amber Lochner <alochner@hawk.iit.edu>
2020-09-14 13:51:07 -0500

    Completed chapter 5 regarding conversions and wrote first full name program

--- ch.5.rb
+++ ch.5.rb
@@ -1,3 +1,37 @@
 var1 = 2
 var2 = '5'
 puts var1.to_s + var2
+
+var1 = 2
+var2 = '5'
+puts var1.to_s + var2
+puts var1 + var2.to_i
+
+puts '15'.to_f
+puts '99.999'.to_f
+puts '99.999'.to_i
+puts ''
+puts '5 is my favorite number!'.to_i
+puts 'Who asked you about 5 or whatever?'.to_i
+puts 'Your momma did.'.to_f
+puts ''
+puts 'stringy'.to_s
+puts 3.to_i
+
+
+puts 20
+puts 20.to_s
+puts '20'
+
+
+
+puts 'What is your first name?'
+first_name = gets.chomp
+puts 'Your first name is ' + first_name + '? What a cool first name!'
+puts 'What is your middle name?'
+middle_name = gets.chomp
+puts 'Your middle name is ' + middle_name + '? Nice!'
+puts 'What is your last name?'
+last_name = gets.chomp
+puts 'Your last name is ' + last_name + '? Awesome!'
+puts 'Nice to meet you, ' + first_name + middle_name + last_name + '. '
