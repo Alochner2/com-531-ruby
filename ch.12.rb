0c519832c1ba03951c9fe76c12a3b08c9ec8fff7
Amber Lochner <alochner@hawk.iit.edu>
2020-10-02 12:21:20 -0500

    Added range method exercise

--- ch.12.rb
+++ ch.12.rb
@@ -27,3 +27,38 @@ puts "What day were you born?"
 day = gets
 
 puts Time = [year, month, day]
+
+dict_array = []
+dict_hash = {}
+dict_array[0] = 'candle'
+dict_array[1] = 'glasses'
+dict_array[2] = 'truck'
+dict_array[3] = 'Alicia'
+dict_hash['shia-a'] = 'candle'
+dict_hash['shaya' ] = 'glasses'
+dict_hash['shasha'] = 'truck'
+dict_hash['sh-sha'] = 'Alicia'
+
+dict_array.each do |word|
+  puts word
+end
+
+dict_hash.each do |c_word, word|
+  puts "#{c_word}: #{word}"
+end
+
+
+letters = 'a'..'c'
+
+puts(['a','b','c'] == letters.to_a)
+
+('A'..'Z').each do |letter| print letter
+end
+puts
+
+god_bless_the_70s = 1970..1979
+puts god_bless_the_70s.min
+puts god_bless_the_70s.max
+puts(god_bless_the_70s.include?(1979 ))
+puts(god_bless_the_70s.include?(1980 ))
+puts(god_bless_the_70s.include?(1974.5))
