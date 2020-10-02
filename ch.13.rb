836624fdf36d0bf28c6e1f8125e848bf9d6ce2e6
Amber Lochner <alochner@hawk.iit.edu>
2020-10-02 14:20:24 -0500

    Learn how to create new classes

---
+++ ch.13.rb
@@ -0,0 +1,44 @@
+class Integer
+  def to_eng
+    if self == 5
+      english = 'five'
+    else
+      english = 'forty-two'
+    end
+
+english
+  end
+end
+
+puts 5.to_eng
+puts 42.to_eng
+
+class Die
+def roll
+@number_showing = 1 + rand(6)
+end
+def showing @number_showing
+end end
+die = Die.new die.roll
+puts die.showing puts die.showing die.roll
+puts die.showing puts die.showing
+
+class Die
+
+  def roll
+    @number_showing = 1 + rand(6)
+  end
+
+  def showing
+    @number_showing
+  end
+
+end
+
+die = Die.new
+die.roll
+puts die.showing
+puts die.showing
+die.roll
+puts die.showing
+puts die.showing
